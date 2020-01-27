5f345edf7b4e3054343096072b06aaf18264924f
patelane <patelane@sheridancollege.ca>
2020-01-27 17:18:49 -0500

    week2 push example

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -23,7 +23,9 @@ public class CardTrick {
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        System.out.println("Enter the suit:");
         
+        System.out.println("Enter the value:");
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here to find matching card is in array or not
         //Then report the result here
